e1aa3fbc8c7312d38e15523d47ffa85c4063cda8
Martin Kapal <flamecze@gmail.com>
2022-11-16 09:58:10 +0100

    Limit log message length

--- src/insim/full/features/packetLogs/log.ts
+++ src/insim/full/features/packetLogs/log.ts
@@ -3,9 +3,21 @@ import { IS_MSL, PacketType } from 'node-insim/packets';
 import type { InSim } from 'node-insim/protocols';
 
 export function log(packet: IPacket, inSim: InSim, text: string) {
+  const prefix = `^7${PacketType[packet.Type]}: ^8`;
+  const msg = `${prefix}${text}`;
+
+  if (msg.length >= 128) {
+    inSim.send(
+      new IS_MSL({
+        Msg: `${msg.substring(0, 124)}...`,
+      }),
+    );
+    return;
+  }
+
   inSim.send(
     new IS_MSL({
-      Msg: `^7${PacketType[packet.Type]}: ^8${text}`,
+      Msg: msg,
     }),
   );
 }
