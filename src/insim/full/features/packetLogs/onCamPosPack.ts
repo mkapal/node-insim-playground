a0d7b2ad87683aba3320f810ff58c5f2ffe098d1
Martin Kapal <flamecze@gmail.com>
2022-11-30 22:51:49 +0100

    More info in IS_CPP log

--- src/insim/full/features/packetLogs/onCamPosPack.ts
+++ src/insim/full/features/packetLogs/onCamPosPack.ts
@@ -1,4 +1,5 @@
 import type { IS_CPP } from 'node-insim/packets';
+import { ViewIdentifier } from 'node-insim/packets';
 import type { InSim } from 'node-insim/protocols';
 
 import { log } from './log';
@@ -7,6 +8,8 @@ export function onCamPosPack(packet: IS_CPP, inSim: InSim) {
   log(
     packet,
     inSim,
-    `Camera position requested: PLID ${packet.ViewPLID}, FOV ${packet.FOV}`,
+    `Camera position requested: PLID ${packet.ViewPLID} - ${
+      ViewIdentifier[packet.InGameCam]
+    } - FOV ${packet.FOV} [${packet.X},${packet.Y},${packet.Z}]`,
   );
 }
