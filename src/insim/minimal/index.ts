23942fbe3b66f37ac32d923d46a0ce372bcdd316
Martin Kapal <flamecze@gmail.com>
2023-02-11 16:55:27 +0100

    Cleanup minimal example, use yalc to connect node-insim

--- src/insim/minimal/index.ts
+++ src/insim/minimal/index.ts
@@ -3,16 +3,15 @@ import NodeInSim from 'node-insim';
 import type { IS_VER } from 'node-insim/packets';
 import { IS_ISI_ReqI, PacketType } from 'node-insim/packets';
 
-const inSim = new NodeInSim.InSim();
-const insimName = 'Node InSim Mini';
 const log = debug('node-insim-minimal');
-const logError = debug('node-insim-minimal:error');
+
+const inSim = new NodeInSim.InSim();
 
 inSim.connect({
+  IName: 'Node InSim Mini',
   Host: process.env.HOST ?? '127.0.0.1',
   Port: process.env.PORT ? parseInt(process.env.PORT) : 29999,
   ReqI: IS_ISI_ReqI.SEND_VERSION,
-  IName: insimName,
 });
 
 inSim.on('connect', () => log('Connected'));
@@ -22,7 +21,3 @@ inSim.on(PacketType.ISP_VER, onVersion);
 function onVersion(packet: IS_VER) {
   log(`Connected to LFS ${packet.Product} ${packet.Version}`);
 }
-
-inSim.on('error', (error) => {
-  logError(`Error:`, error);
-});
